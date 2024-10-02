f52a4e70ddb3d183ba3c2eddd7eee4163ad07c39
Daniil Pavlovskyi <s25176@pjwstk.edu.pl>
2024-10-02 12:19:38 +0200

    UserService init
    user service init

---
+++ UserService/src/main/java/com/dp/userservice/security/SecurityConfig.java
@@ -0,0 +1,94 @@
+package com.dp.userservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.dp.userservice.security.Roles.ADMIN;
+import static com.dp.userservice.security.Roles.USER;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+enum Roles {
+    ADMIN("ADMIN"), USER("USER");
+
+    Roles(String name) {}
+}
+
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Configuration
+public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final UserDetailsService userDetailsService;
+
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers(
+                                    "/api/auth/",
+                                    "/api/auth/*",
+                                    "api/report/types",
+                                    "/wander_wave_swagger",
+                                    "/api-docs",
+                                    "/v2/api-docs",
+                                    "/v3/api-docs",
+                                    "/v3/api-docs/**",
+                                    "/swagger-resources",
+                                    "/swagger-resources/**",
+                                    "/configuration/ui",
+                                    "/configuration/security",
+                                    "/swagger-ui/**",
+                                    "/webjars/**",
+                                    "/swagger-ui.html").permitAll()
+                            .requestMatchers(
+                                    "/api/reports/find-by-dates",
+                                    "/api/reports/review").hasRole(ADMIN.name())
+                            .requestMatchers(
+                                    "api/reports/send").hasRole(USER.name())
+                            .anyRequest().authenticated();
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager
+            (AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+}
